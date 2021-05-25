a930b7a39b5816985825b578318195510ce4481f
rla5451 <rla5451@naver.com>
2021-05-26 00:05:22 +0900

    레벨테스트 추가

---
+++ BU_EDU/FormLevel.cpp
@@ -0,0 +1,49 @@
+﻿// FormLevel.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "BU_EDU.h"
+#include "FormLevel.h"
+
+
+// FormLevel
+
+IMPLEMENT_DYNCREATE(FormLevel, CFormView)
+
+FormLevel::FormLevel()
+	: CFormView(IDD_LEVEL)
+{
+
+}
+
+FormLevel::~FormLevel()
+{
+}
+
+void FormLevel::DoDataExchange(CDataExchange* pDX)
+{
+	CFormView::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(FormLevel, CFormView)
+END_MESSAGE_MAP()
+
+
+// FormLevel 진단
+
+#ifdef _DEBUG
+void FormLevel::AssertValid() const
+{
+	CFormView::AssertValid();
+}
+
+#ifndef _WIN32_WCE
+void FormLevel::Dump(CDumpContext& dc) const
+{
+	CFormView::Dump(dc);
+}
+#endif
+#endif //_DEBUG
+
+
+// FormLevel 메시지 처리기
